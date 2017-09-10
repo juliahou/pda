53e818d874a20d3de0945e37da50502f7cd60ca3
juliahou8 <jjhou@andrew.cmu.edu>
2017-09-10 04:48:32 -0400

    Everything would work if it weren't for the dumb push tokens

--- notifications/app/src/server.py
+++ notifications/app/src/server.py
@@ -16,12 +16,36 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 db = SQLAlchemy(app)
 app.config['SECRET_KEY'] = 'meow'
 
+# class JsonEncodedDict(db.TypeDecorator):
+#     """Enables JSON storage by encoding and decoding on the fly."""
+#     impl = db.Text
+
+#     def process_bind_param(self, value, dialect):
+#         if value is None:
+#             return '{}'
+#         else:
+#             return json.dumps(value)
+
+#     def process_result_value(self, value, dialect):
+#         if value is None:
+#             return {}
+#         else:
+#             return json.loads(value)
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True)
     password = db.Column(db.String(80), unique=False)
-    token = db.Column(db.String(100), unique=True)
-    location = db.Column(db.String(100), unique=False)
+    # token = db.Column(JsonEncodedDict)
+    # location = db.Column(JsonEncodedDict)
+    token = db.Column(db.Text, unique=True)
+    location = db.Column(db.Text, unique=False)
+
+    def __init__(self, username='', password='', token='', location=''):
+        self.username = username
+        self.password = password
+        self.token = token
+        self.location = location
 
 def init_db():
     db.create_all()
@@ -34,13 +58,53 @@ def init_db():
 
 # Basic arguments. You should extend this function with the push features you
 # want to use, or simply pass in a `PushMessage` object.
-def send_push_message(token, message):
-#     # try:
-    response = PushClient().publish(PushMessage(to=token,body=message))
+def send_push_message(token, message, extra=None):
+    try:
+        response = PushClient().publish(
+            PushMessage(to=token,
+                        body=message,
+                        data=extra))
+    except PushServerError as exc:
+        # Encountered some likely formatting/validation error.
+        rollbar.report_exc_info(
+            extra_data={
+                'token': token,
+                'message': message,
+                'extra': extra,
+                'errors': exc.errors,
+                'response_data': exc.response_data,
+            })
+        raise
+    except (ConnectionError, HTTPError) as exc:
+        # Encountered some Connection or HTTP error - retry a few times in
+        # case it is transient.
+        rollbar.report_exc_info(
+            extra_data={'token': token, 'message': message, 'extra': extra})
+        raise self.retry(exc=exc)
+
+    try:
+        # We got a response back, but we don't know whether it's an error yet.
+        # This call raises errors so we can handle them with normal exception
+        # flows.
+        response.validate_response()
+    except DeviceNotRegisteredError:
+        # Mark the push token as inactive
+        from notifications.models import PushToken
+        PushToken.objects.filter(token=token).update(active=False)
+    except PushResponseError as exc:
+        # Encountered some other per-notification error.
+        rollbar.report_exc_info(
+            extra_data={
+                'token': token,
+                'message': message,
+                'extra': extra,
+                'push_response': exc.push_response._asdict(),
+            })
+        raise self.retry(exc=exc)
 
 @app.route('/distress', methods=['POST'])
 def distress():
-    users = User.query
+    users = User.query.all()
     json = request.get_json()
     for user in users:
         if json['id'] != user.id:
@@ -55,41 +119,4 @@ def add_user():
     new_user.display_name = 'cat'
     db.session.add(new_user)
     db.session.commit()
-
-    # except PushServerError as exc:
-    #     # Encountered some likely formatting/validation error.
-    #     rollbar.report_exc_info(
-    #         extra_data={
-    #             'token': token,
-    #             'message': message,
-    #             'extra': extra,
-    #             'errors': exc.errors,
-    #             'response_data': exc.response_data,
-    #         })
-    #     raise
-    # except (ConnectionError, HTTPError) as exc:
-    #     # Encountered some Connection or HTTP error - retry a few times in
-    #     # case it is transient.
-    #     rollbar.report_exc_info(
-    #         extra_data={'token': token, 'message': message, 'extra': extra})
-    #     raise self.retry(exc=exc)
-
-    # try:
-    #     # We got a response back, but we don't know whether it's an error yet.
-    #     # This call raises errors so we can handle them with normal exception
-    #     # flows.
-    #     response.validate_response()
-    # except DeviceNotRegisteredError:
-    #     # Mark the push token as inactive
-    #     from notifications.models import PushToken
-    #     PushToken.objects.filter(token=token).update(active=False)
-    # except PushResponseError as exc:
-    #     # Encountered some other per-notification error.
-    #     rollbar.report_exc_info(
-    #         extra_data={
-    #             'token': token,
-    #             'message': message,
-    #             'extra': extra,
-    #             'push_response': exc.push_response._asdict(),
-    #         })
-    #     raise self.retry(exc=exc)
+    return ('', 204)
