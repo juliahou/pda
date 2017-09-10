063bc9ecb12b0023eebe0892364301166c085dcc
juliahou8 <jjhou@andrew.cmu.edu>
2017-09-09 23:14:52 -0400

    Still doesn't work but server logic makes sense??

--- notifications/app/src/server.py
+++ notifications/app/src/server.py
@@ -1,5 +1,6 @@
-from src import app
-from flask import jsonify, request
+# from src import app
+from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
 import requests
 import json
 from exponent_server_sdk import DeviceNotRegisteredError
@@ -10,15 +11,49 @@ from exponent_server_sdk import PushServerError
 from requests.exceptions import ConnectionError
 from requests.exceptions import HTTPError
 
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+db = SQLAlchemy(app)
+app.config['SECRET_KEY'] = 'meow'
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+    password = db.Column(db.String(80), unique=False)
+    token = db.Column(db.String(100), unique=True)
+    location = db.Column(db.String(100), unique=False)
+
+def init_db():
+    db.create_all()
+
+    # Create a test user
+    # new_user = User('jjhou', 'meow', 'dg', 'erg')
+    # new_user.display_name = 'Julia'
+    # db.session.add(new_user)
+    # db.session.commit()
 
 # Basic arguments. You should extend this function with the push features you
 # want to use, or simply pass in a `PushMessage` object.
-def send_push_message(token, message, extra=None):
-    # try:
-    response = PushClient().publish(
-        PushMessage(to=token,
-                    body=message,
-                    data=extra))
+def send_push_message(token, message):
+#     # try:
+    response = PushClient().publish(PushMessage(to=token,body=message))
+
+@app.route('/distress', methods=['POST'])
+def distress():
+    users = User.query
+    json = request.get_json()
+    for user in users:
+        if json['id'] != user.id:
+            send_push_message(user.token, "WOOO PUSH NOTIFICATION")
+
+@app.route('/add_user', methods=['POST'])
+def add_user():
+    json = request.get_json()
+    new_user = User(json['username'], json['password'], json['token'], json['location'])
+    new_user.display_name = 'cat'
+    db.session.add(new_user)
+    db.session.commit()
+
     # except PushServerError as exc:
     #     # Encountered some likely formatting/validation error.
     #     rollbar.report_exc_info(
